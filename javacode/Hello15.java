b1cac1d84f8148378a978e987d59f44a365ac6b5
VincentTuan <vincentjava@yahoo.com.tw>
2025-07-19 12:10:36 +0800

    Add files via upload

--- javacode/Hello15.java
+++ javacode/Hello15.java
@@ -6,19 +6,38 @@ import java.util.Scanner;
 // 請考慮紙鈔與硬幣面額
 public class Hello15 {
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in); // System.in = 鍵盤
+
 		// 商品
 		int price = 123; // 飲料價格
 		System.out.printf("飲料價格:%d%n", price);
+
+		// 數量
+		System.out.print("請輸入購買數量:");
+		int amount = scanner.nextInt();
+
+		// 應付金額
+		int total = price * amount;
+		System.out.printf("應付款金額:%d%n", total);
 		
 		// 付款(支援手動輸入)
 		System.out.print("請輸入付款金額:");
-		Scanner scanner = new Scanner(System.in); // System.in = 鍵盤
+		
 		int pay = scanner.nextInt(); // 取得使用者在鍵盤上所輸入數字
 
 		// 找零
-		int change = pay - price;
+		int change = pay - total;
+		
+		// 檢查 change 是否 < 0
+		if(change < 0) {
+			System.out.println("付款金額不足");
+			return; // 此方法結束(下面的程式就不執行)
+		}
+
 		System.out.printf("找零:%d%n", change);
 		System.out.println("-----------------");
+		
+		// 如何找錢
 		int fiveHundred = change / 500;
 		change = change % 500;
 
